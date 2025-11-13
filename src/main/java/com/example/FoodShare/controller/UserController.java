5dc3e2b310c9ec643711b8e421ab6801ac38719c
sonal <misssonalianil08@gmail.com>
2025-11-13 14:47:13 +0530

    Initial commit - FoodShare spring Boot project

--- src/main/java/com/example/FoodShare/controller/UserController.java
+++ src/main/java/com/example/FoodShare/controller/UserController.java
@@ -1,11 +1,14 @@
 package com.example.FoodShare.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import com.example.FoodShare.entity.User;
 import com.example.FoodShare.repository.UserRepository;
 
@@ -13,27 +16,39 @@ import com.example.FoodShare.repository.UserRepository;
 @RestController
 @RequestMapping("/api")
 public class UserController {
-	
-	@Autowired
-	private UserRepository userRepository;
-	
-	@PostMapping("/signup")
-	public String signup(@RequestBody User user) {
-		if (userRepository.findByEmail(user.getEmail()) != null) {
-			return "Email already registered";
-		}
-		userRepository.save(user);
-		return "User registered successfully";
-	}
-	
-	@PostMapping("/login")
-	public String login(@RequestBody User user) {
-		User existingUser = userRepository.findByEmail(user.getEmail());
-		if (existingUser != null && existingUser.getPassword().equals(user.getPassword())) {
-            return "Login successful!";
-        } else {
-            return "Invalid email or password!";
-        }
-	}
 
+    @Autowired
+    private UserRepository userRepository;
+
+    
+    private final BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+
+    @PostMapping("/signup")
+    public ResponseEntity<String> signup(@RequestBody User user) {
+        if (userRepository.findByEmail(user.getEmail()) != null) {
+            return ResponseEntity.status(HttpStatus.CONFLICT).body("Email already registered!");
+        }
+
+       
+        user.setPassword(encoder.encode(user.getPassword()));
+
+        userRepository.save(user);
+        return ResponseEntity.status(HttpStatus.CREATED).body("User registered successfully!");
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<String> login(@RequestBody User user) {
+        User existingUser = userRepository.findByEmail(user.getEmail());
+
+        if (existingUser == null) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Email not registered!");
+        }
+
+      
+        if (!encoder.matches(user.getPassword(), existingUser.getPassword())) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid password!");
+        }
+
+        return ResponseEntity.ok("Login successful!");
+    }
 }
